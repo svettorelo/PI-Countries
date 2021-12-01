63bbe3d9a3e6aa7f64d46a9d119c5a4f88a65ffb
svettorelo <69180670+svettorelo@users.noreply.github.com>
2021-12-01 18:00:45 -0300

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
-export const COUNTRIES_URL = 'http://localhost:3001/countries';
+export const COUNTRIES_URL = "https://my-app-countries.herokuapp.com/countries";            //'http://localhost:3001/countries';
 
-export const ACTIVITY_URL = 'http://localhost:3001/activity';
+export const ACTIVITY_URL = "https://my-app-countries.herokuapp.com/activity";              //'http://localhost:3001/activity';
 
 //const NAME_URL = 'https://restcountries.com/v3/name' //https://restcountries.com/v2/name/{name}
 //const ALPHA_URL = 'https://restcountries.com/v3/alpha' // GET https://restcountries.com/v2/alpha/{code}
